52fdab97c0bc6c5f95db5b5a23a300cec850deb2
Will-Harris00 <wjharris00@outlook.com>
2021-07-04 22:12:48 +0100

    Finalised glider function

--- Task 2 - Haskell/gliderAlt.hs
+++ Task 2 - Haskell/gliderAlt.hs
@@ -1,7 +1,40 @@
+type Point
+    = (Int, Int)
+
+glider :: [ Point ]
+glider
+    = [ (0, 2), (1, 3), (2, 1), (2, 2), (2, 3) ]
+
 generateGrid :: Int -> Int -> String -> [ String ]
 generateGrid width height 
     = replicate height . concat . replicate width
 
+placeHash :: Point -> [String] -> [String]
+placeHash (x, y) zs
+    = changeItemAt zs y repl
+    where
+        repl = changeItemAt (zs!!y) x '#'
+
+changeItemAt :: [a] -> Int -> a -> [a]
+changeItemAt xs i s
+    = h ++ [s] ++ (tail t)
+    where
+        (h, t) = splitAt i xs
+
+forListOfPoints :: [String] -> [Point] -> [String]
+forListOfPoints a []
+    = a
+forListOfPoints gr (x:xs)
+    = forListOfPoints replaced xs
+    where 
+        replaced = placeHash x gr
+
+visualisation :: Int -> Int -> [ [ Point ] ] -> [ [ String ] ]
+visualisation w h gs
+    = map (forListOfPoints grid) gs
+    where
+        grid = generateGrid w h "."
+
 main :: IO()
 main
-    = putStrLn (show (generateGrid 5 5 "."))
+    = putStrLn (show (visualisation 5 5 [glider]))
