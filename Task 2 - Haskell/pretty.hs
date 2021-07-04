c1a3f256d83599d19b988fa38d44a468d628ba01
Will-Harris00 <wjharris00@outlook.com>
2021-07-04 20:15:28 +0100

    Reintroduced intersperse prelude function

--- Task 2 - Haskell/pretty.hs
+++ Task 2 - Haskell/pretty.hs
@@ -1,9 +1,16 @@
-import Data.List ( intercalate )
+import Data.List ( intercalate, intersperse )
 
-pretty :: Show a => [[[a]]] -> String
+pretty :: Show a => [[[a]]] -> [ String ]
 pretty xs
-    -- = strip . (++ "\n") . intercalate "\n" . map show $ concat xs
-    = (++ "\n") . strip . intercalate "\n" . map show $ concat xs
+    = intersperse "\n" . map show $ xs
+
+prettyalt :: Show a => [[[a]]] -> String
+prettyalt xs
+    -- = intercalate "\n" . map show $ xs
+    -- = intercalate "\n" . map show $ concat xs
+    -- = (++ "\n") . strip . intercalate "\n" . map show $ xs
+    -- = (++ "\n") . strip . intercalate "\n" . map show $ concat xs
+    = intercalate "\n" . map show $ (intercalate "\n" . map show $ xs)
 
 strip :: String -> String
 strip xs
@@ -11,6 +18,14 @@ strip xs
 
 main :: IO()
 main
-    = putStrLn (show (pretty ([ [ [ 'a','b' ], [ 'c','d' ] ],
-                                [ [ 'e','f' ], [ 'g','h' ] ],
-                                [ [ 'i','j' ], [ 'k','l' ] ] ])))
+    = putStrLn (show (prettyalt ([ [".....","..#..","#.#..",".##..","....."],
+                                   [".....",".#...","..##.",".##..","....."],
+                                   [".....","..#..","...#.",".###.","....."],
+                                   [".....",".....",".#.#.","..##.","..#.."],
+                                   [".....",".....","...#.",".#.#.","..##."],
+                                   [".....",".....","..#..","...##","..##."],
+                                   [".....",".....","...#.","....#","..###"] ])))
+    {-= putStrLn (show (pretty ([ [ [ 'a','b' ], [ 'c','d' ] ],
+                                  [ [ 'e','f' ], [ 'g','h' ] ],
+                                  [ [ 'i','j' ], [ 'k','l' ] ] ])))
+    -}
