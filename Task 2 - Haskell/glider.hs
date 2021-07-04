1cf718dcf64342afd302a9519883f4a8d5138e6b
Will-Harris00 <wjharris00@outlook.com>
2021-07-04 21:26:18 +0100

    Replicated first row to form board grid

--- Task 2 - Haskell/glider.hs
+++ Task 2 - Haskell/glider.hs
@@ -5,10 +5,10 @@ glider :: [ Point ]
 glider
     = [ (0, 2), (1, 3), (2, 1), (2, 2), (2, 3) ]
 
-visualisation :: Int -> String
-visualisation x
-    = replicate x '.'
+visualisation :: Int -> Int -> [ [ String ] ]
+visualisation x y
+    = [replicate y $ (replicate x '.')]
 
 main :: IO()
 main
-    = putStrLn (show (visualisation 5))
+    = putStrLn (show (visualisation 5 5))
