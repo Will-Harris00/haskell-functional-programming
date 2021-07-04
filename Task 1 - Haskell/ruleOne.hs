4fa55b837162cce5f84b348ce5f59670951277ef
Will-Harris00 <wjharris00@outlook.com>
2021-07-04 16:36:17 +0100

    Code Styling

--- Task 1 - Haskell/ruleOne.hs
+++ Task 1 - Haskell/ruleOne.hs
@@ -2,11 +2,10 @@ rule1 :: (Int,Int,Int,Int,Int,Int) -> Bool
 
 rule1 (t1,t2,t3,t4,t5,t6) 
     = t1 /= t2 && t1 /= t3 && t1 /= t4 && t1 /= t5 && t1 /= t6
-    && t2 /= t1 && t2 /= t3 && t2 /= t4 && t2 /= t5 && t2 /= t6
-    && t3 /= t1 && t3 /= t2 && t3 /= t4 && t3 /= t5 && t3 /= t6
-    && t4 /= t1 && t4 /= t2 && t4 /= t3 && t4 /= t5 && t4 /= t6
-    && t5 /= t1 && t5 /= t2 && t5 /= t3 && t5 /= t4 && t5 /= t6
-    && t6 /= t1 && t6 /= t2 && t6 /= t3 && t6 /= t4 && t6 /= t5
+    && t2 /= t3 && t2 /= t4 && t2 /= t5 && t2 /= t6
+    && t3 /= t4 && t3 /= t5 && t3 /= t6
+    && t4 /= t5 && t4 /= t6
+    && t5 /= t6
 
 main :: IO()
 main
