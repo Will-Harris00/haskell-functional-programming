af518f3250c80e21bb8f06dd620cdf47bf67d5a4
Will-Harris00 <wjharris00@outlook.com>
2021-07-04 16:50:00 +0100

    Check alternate digits differ by more than two

---
+++ Task 1 - Haskell/ruleThree.hs
@@ -0,0 +1,14 @@
+rule3 :: (Int,Int,Int,Int,Int,Int) -> Bool
+rule3 (t1,t2,t3,t4,t5,t6)
+    = isDiffTwo (t1, t2) && isDiffTwo (t2, t3) && isDiffTwo (t3, t4) 
+    && isDiffTwo (t4, t5) && isDiffTwo (t5, t6)
+
+isDiffTwo :: (Int, Int) -> Bool
+isDiffTwo (n, m)
+    | n - m < -2 || n - m > 2 = True
+    | otherwise = False
+
+main :: IO()
+main
+    = putStrLn (show (rule3 (4,9,6,3,0,7)))
+    -- = putStrLn (show (rule3 (6,9,2,7,0,3)))
