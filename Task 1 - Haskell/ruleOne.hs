e5f7bf0dbe122eec2e1e607ad72778f19705119f
Will-Harris00 <wjharris00@outlook.com>
2021-07-04 16:34:03 +0100

    Check all digits are unique

---
+++ Task 1 - Haskell/ruleOne.hs
@@ -0,0 +1,15 @@
+rule1 :: (Int,Int,Int,Int,Int,Int) -> Bool
+
+rule1 (t1,t2,t3,t4,t5,t6) 
+    = t1 /= t2 && t1 /= t3 && t1 /= t4 && t1 /= t5 && t1 /= t6
+    && t2 /= t1 && t2 /= t3 && t2 /= t4 && t2 /= t5 && t2 /= t6
+    && t3 /= t1 && t3 /= t2 && t3 /= t4 && t3 /= t5 && t3 /= t6
+    && t4 /= t1 && t4 /= t2 && t4 /= t3 && t4 /= t5 && t4 /= t6
+    && t5 /= t1 && t5 /= t2 && t5 /= t3 && t5 /= t4 && t5 /= t6
+    && t6 /= t1 && t6 /= t2 && t6 /= t3 && t6 /= t4 && t6 /= t5
+
+main :: IO()
+main
+    = putStrLn (show (rule1 (1,2,3,4,5,6)))
+    -- = putStrLn (show (rule1 (4,9,6,3,0,7)))
+    -- = putStrLn (show (rule1 (6,9,2,7,0,3)))
