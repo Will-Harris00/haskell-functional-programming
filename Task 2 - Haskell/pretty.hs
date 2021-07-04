03631c55f3088708ccc0cf57c0ac6091b410a2d8
Will-Harris00 <wjharris00@outlook.com>
2021-07-04 19:58:56 +0100

    Using derivation of show to alter formatting

--- Task 2 - Haskell/pretty.hs
+++ Task 2 - Haskell/pretty.hs
@@ -2,10 +2,18 @@ import Data.List ( intersperse, intercalate )
 
 prettyalt :: Show a => [[[a]]] -> String
 prettyalt xs
-    = (concat . intersperse "\n" . map show $ concat xs)
+    = (intercalate "\n" . map show $ concat xs)
+
+midstep :: Show a => [[[a]]] -> [String]
+midstep xs
+    = map show $ concat xs
 
 main :: IO()
 main
     = putStrLn (show (prettyalt ([ [ [ 'a','b' ], [ 'c','d' ] ],
                                    [ [ 'e','f' ], [ 'g','h' ] ],
                                    [ [ 'i','j' ], [ 'k','l' ] ] ])))
+    {-= putStrLn (show (midstep ([ [ [ 'a','b' ], [ 'c','d' ] ],
+                                   [ [ 'e','f' ], [ 'g','h' ] ],
+                                   [ [ 'i','j' ], [ 'k','l' ] ] ])))
+    -}
