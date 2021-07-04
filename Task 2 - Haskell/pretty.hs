541daf86ad263c7c151368e8a23545beaf9403f8
Will-Harris00 <wjharris00@outlook.com>
2021-07-04 20:11:39 +0100

    Simplified pretty print process

--- Task 2 - Haskell/pretty.hs
+++ Task 2 - Haskell/pretty.hs
@@ -1,26 +1,9 @@
 import Data.List ( intercalate )
 
-prettyalt :: Show a => [[[a]]] -> String
-prettyalt xs
-    = (intercalate "\n" . map show $ concat xs)
-
-midstep :: Show a => [[[a]]] -> [String]
-midstep xs
-    = map show $ concat xs
-
-firststep :: [[[a]]] -> [[a]]
-firststep xs
-    = concat xs
-
-firststepalt :: [[a]] -> [a]
-firststepalt xs
-    = foldr (++) [] xs
-
 pretty :: Show a => [[[a]]] -> String
 pretty xs
-    = strip . (++ "\n") . intercalate "\n" . map show $ concat xs
-    -- = (++ "\n") . strip . intercalate "\n" . map show $ concat xs
-    
+    -- = strip . (++ "\n") . intercalate "\n" . map show $ concat xs
+    = (++ "\n") . strip . intercalate "\n" . map show $ concat xs
 
 strip :: String -> String
 strip xs
@@ -31,15 +14,3 @@ main
     = putStrLn (show (pretty ([ [ [ 'a','b' ], [ 'c','d' ] ],
                                 [ [ 'e','f' ], [ 'g','h' ] ],
                                 [ [ 'i','j' ], [ 'k','l' ] ] ])))
-    {-= putStrLn (show (firststepalt ([ [ [ 'a','b' ], [ 'c','d' ] ],
-                                        [ [ 'e','f' ], [ 'g','h' ] ],
-                                        [ [ 'i','j' ], [ 'k','l' ] ] ])))
-    -}
-    {-= putStrLn (show (firststep ([ [ [ 'a','b' ], [ 'c','d' ] ],
-                                     [ [ 'e','f' ], [ 'g','h' ] ],
-                                     [ [ 'i','j' ], [ 'k','l' ] ] ])))
-    -}
-    {-= putStrLn (show (midstep ([ [ [ 'a','b' ], [ 'c','d' ] ],
-                                   [ [ 'e','f' ], [ 'g','h' ] ],
-                                   [ [ 'i','j' ], [ 'k','l' ] ] ])))
-    -}
