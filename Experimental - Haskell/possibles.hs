450b3f9ee7a859ff51e61b519159e9a62006bd89
Will-Harris00 <wjharris00@outlook.com>
2021-07-05 13:25:36 +0100

    Altered number generation

--- Experimental - Haskell/possibles.hs
+++ Experimental - Haskell/possibles.hs
@@ -1,28 +1,20 @@
-countToN :: Int -> [Int] -> [(Int,Int,Int,Int,Int,Int)] -> [(Int,Int,Int,Int,Int,Int)]
-countToN x xs ys
-    | x /= 9999 = countToN (succ x) (xs ++ [x]) $ ys ++ [listToTuple . stringToList . padOut . succ $ x]
+countToN :: Int -> [(Int, Int, Int, Int, Int, Int)] -> [(Int, Int, Int, Int, Int, Int)]
+countToN x ys
+    | x /= 9999 = countToN (succ x) (ys ++ [listToTuple . stringToList . padOut . succ $ x])
     | otherwise = ys
-
-intToString :: Int -> String
-intToString x 
-    = show x
-
-stringToInt :: String -> Int
-stringToInt x
-    = read x :: Int
 
 padOut :: Int -> String
 padOut x
-    = replicate (6 - length (intToString x)) '0' ++ intToString x
+    = replicate (6 - length (show x)) '0' ++ show x
 
 stringToList :: String -> [Int]
 stringToList x
     = map (read . (:"")) x :: [Int]
 
-listToTuple :: [Int] -> (Int,Int,Int,Int,Int,Int)
-listToTuple [t1,t2,t3,t4,t5,t6]
-    = (t1,t2,t3,t4,t5,t6)
+listToTuple :: [Int] -> (Int, Int, Int, Int, Int, Int)
+listToTuple [x1, x2, x3, x4, x5, x6]
+    = (x1, x2, x3, x4, x5, x6)
 
 main :: IO()
 main
-    = putStrLn (show (countToN (-1) [] []))
+    = putStrLn (show (countToN (-1) []))
