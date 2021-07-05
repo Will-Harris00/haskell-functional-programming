288c8e7fbad4295637281aab00a7e9fd59caca3d
Will-Harris00 <wjharris00@outlook.com>
2021-07-05 13:51:26 +0100

    Attempted to generate sequential tuples

--- Experimental - Haskell/possibles.hs
+++ Experimental - Haskell/possibles.hs
@@ -1,41 +1,41 @@
-{-
 countToN :: [Int]
 countToN
-    = listString . listInt $ prnt
+    = listInt $ prnt
     -- = [listToTuple . listString . listInt $ prnt]
 
 prnt :: [Int]
 prnt
     = [0..999999]
 
-listInt :: [Int] -> [String]
+listInt :: [Int] -> [Int]
 listInt (x:xs)
-    = padOut x : listInt xs
+    = stringToList . padOut $ x : listInt xs
 
 padOut :: Int -> String
 padOut x
     = replicate (6 - length (show x)) '0' ++ show x
 
-stringToList :: String -> Int
+stringToList :: String -> [Int]
 stringToList x
-    = map (read . (:"")) x :: Int
-
-listString :: [String] -> [Int]
-listString (x:xs)
-    = stringToList x : listString xs
+    = map (read . (:"")) x :: [Int]
 
 listToTuple :: [Int] -> (Int, Int, Int, Int, Int, Int)
 listToTuple [x1, x2, x3, x4, x5, x6]
     = (x1, x2, x3, x4, x5, x6)
--}
+
+{-
+listString :: [String] -> [Int]
+listString (x:xs)
+    = testString x : listString xs
 
 testString :: String -> [Int]
 testString x
     = map (read . (:"")) x :: [Int]
+-}
 
 main :: IO()
 main
     -- = putStrLn (show (prnt))
-    -- = putStrLn (show (countToN))
-    = putStrLn (show (testString "999999"))
+    = putStrLn (show (countToN))
+    -- = putStrLn (show (testString ["999999"]))
     -- = putStrLn (show $ findSolutions $ possibles)
