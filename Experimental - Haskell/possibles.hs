13ce228d56bbf0c0c1e74f4823592bec5a0a84a4
Will-Harris00 <wjharris00@outlook.com>
2021-07-05 13:19:26 +0100

    Create six-digit tuples

--- Experimental - Haskell/possibles.hs
+++ Experimental - Haskell/possibles.hs
@@ -1,8 +1,28 @@
-countToN :: Int -> [Int] -> [Int]
-countToN x xs
-    | x /= 1000 = countToN (succ x) (xs ++ [x])
-    | otherwise = xs
+countToN :: Int -> [Int] -> [(Int,Int,Int,Int,Int,Int)] -> [(Int,Int,Int,Int,Int,Int)]
+countToN x xs ys
+    | x /= 9999 = countToN (succ x) (xs ++ [x]) $ ys ++ [listToTuple . stringToList . padOut . succ $ x]
+    | otherwise = ys
+
+intToString :: Int -> String
+intToString x 
+    = show x
+
+stringToInt :: String -> Int
+stringToInt x
+    = read x :: Int
+
+padOut :: Int -> String
+padOut x
+    = replicate (6 - length (intToString x)) '0' ++ intToString x
+
+stringToList :: String -> [Int]
+stringToList x
+    = map (read . (:"")) x :: [Int]
+
+listToTuple :: [Int] -> (Int,Int,Int,Int,Int,Int)
+listToTuple [t1,t2,t3,t4,t5,t6]
+    = (t1,t2,t3,t4,t5,t6)
 
 main :: IO()
 main
-    = putStrLn (show (countToN 0 []))
+    = putStrLn (show (countToN (-1) [] []))
