50be7bf2c5448d84f55050d5ae8929b2b94a5a66
Will-Harris00 <wjharris00@outlook.com>
2021-07-05 13:48:36 +0100

    String number to six-digit tuple

--- Experimental - Haskell/possibles.hs
+++ Experimental - Haskell/possibles.hs
@@ -1,6 +1,8 @@
+{-
 countToN :: [Int]
 countToN
     = listString . listInt $ prnt
+    -- = [listToTuple . listString . listInt $ prnt]
 
 prnt :: [Int]
 prnt
@@ -14,16 +16,26 @@ padOut :: Int -> String
 padOut x
     = replicate (6 - length (show x)) '0' ++ show x
 
+stringToList :: String -> Int
+stringToList x
+    = map (read . (:"")) x :: Int
+
 listString :: [String] -> [Int]
-listString
-    = map (read::String->Int)
+listString (x:xs)
+    = stringToList x : listString xs
 
 listToTuple :: [Int] -> (Int, Int, Int, Int, Int, Int)
 listToTuple [x1, x2, x3, x4, x5, x6]
     = (x1, x2, x3, x4, x5, x6)
+-}
+
+testString :: String -> [Int]
+testString x
+    = map (read . (:"")) x :: [Int]
 
 main :: IO()
 main
     -- = putStrLn (show (prnt))
-    = putStrLn (show (countToN))
+    -- = putStrLn (show (countToN))
+    = putStrLn (show (testString "999999"))
     -- = putStrLn (show $ findSolutions $ possibles)
